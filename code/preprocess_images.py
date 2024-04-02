c2898eeb5783013bd03867c5fdbdc50c00ad33f6
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-02 18:01:23 -0400

    Create preprocess_images.py

---
+++ code/preprocess_images.py
@@ -0,0 +1,11 @@
+from keras.preprocessing import image
+import numpy as np
+from PIL import Image
+
+
+
+def preprocessor(img_path):
+        img = Image.open(img_path).convert("RGB").resize((224,224))
+        img = (np.float32(img)-1.)/(255-1.)
+        img=img.reshape((224,224,3))
+        return img
