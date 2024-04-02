b7000e06e119b156d7c15341a3a4fcf8a2c65ccd
katemcg <59787191+katemcg@users.noreply.github.com>
2024-04-02 19:28:05 -0400

    Update preprocess_images.py
    formatting

--- code/preprocess_images.py
+++ code/preprocess_images.py
@@ -2,8 +2,6 @@ from keras.preprocessing import image
 import numpy as np
 from PIL import Image
 
-
-
 def preprocessor(img_path):
         img = Image.open(img_path).convert("RGB").resize((224,224))
         img = (np.float32(img)-1.)/(255-1.)
