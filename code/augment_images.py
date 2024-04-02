0359bebff2960671cd8e889c685014341c464790
katemcg <59787191+katemcg@users.noreply.github.com>
2024-04-02 19:27:51 -0400

    Update augment_images.py
    adding second function

--- code/augment_images.py
+++ code/augment_images.py
@@ -1,6 +1,14 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def image_data_generator():
+def train_data_generator():
+  return ImageDataGenerator(
+    zoom_range = 0.1, # apply random zoom transformations
+    rotation_range = 15, # randomly rotate images by 15 degrees
+    width_shift_range = .05, # randomly shift images horizontally by 5% of the width
+    height_shift_range = .05 # randomly shift images vertically by 5% of the height
+  )
+
+def val_data_generator():
   return ImageDataGenerator(
     zoom_range = 0.1, # apply random zoom transformations
     rotation_range = 15, # randomly rotate images by 15 degrees
