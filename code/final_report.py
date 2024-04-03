34b506359674b618a4956cd908d1e5c3a14055f8
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-03 12:52:11 -0400

    Update final_report.py

--- code/final_report.py
+++ code/final_report.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from predict_images import predict_image_categories
 from preprocess_images import preprocessor
+from augment_images import data_generator, augmented_images
 
 
 # Define paths
@@ -9,9 +10,11 @@ model_path = '' # chosen model
 
 # Preprocess images
 preprocessed_images = preprocessor(image_paths)
+datagen = data_generator()
+augmented_images = (preprocessed_images, datagen)
 
 # Make predictions
-predictions = predict_image_categories(image_paths, model_path)
+predictions = predict_image_categories(augmented_images, model_path)
 
 # Display predictions
 for image_path, prediction in zip(image_paths, predictions):
