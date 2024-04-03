c79b74ba76e2c15d67c016f5eb0d5c34f332279f
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-03 12:19:53 -0400

    Update evaluate_model.py

--- code/evaluate_model.py
+++ code/evaluate_model.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
-def evaluate_model(model):
+def eval_model(model):
   test_metrics = []
   test_loss, test_accuracy, test_precision, test_recall = model.evaluate(X_test, y_test, verbose=1)
   test_metrics.append({'Test Loss': test_loss,
