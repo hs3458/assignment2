5a75d71d1ba193ea218ee290adedb0f418521987
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-03 12:51:56 -0400

    Update augment_images.py

--- code/augment_images.py
+++ code/augment_images.py
@@ -1,17 +1,18 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
-def train_data_generator():
-  return ImageDataGenerator(
+def data_generator():
+  datagen= ImageDataGenerator(
     zoom_range = 0.1, # apply random zoom transformations
     rotation_range = 15, # randomly rotate images by 15 degrees
     width_shift_range = .05, # randomly shift images horizontally by 5% of the width
-    height_shift_range = .05 # randomly shift images vertically by 5% of the height
-  )
+    height_shift_range = .05 # randomly shift images vertically by 5% of the height)
+    return datagen
 
-def val_data_generator():
-  return ImageDataGenerator(
-    zoom_range = 0.1, # apply random zoom transformations
-    rotation_range = 15, # randomly rotate images by 15 degrees
-    width_shift_range = .05, # randomly shift images horizontally by 5% of the width
-    height_shift_range = .05 # randomly shift images vertically by 5% of the height
-  )
+def augment_images(img, datagen):
+    img = np.expand_dims(img, axis=0)
+    # Generate augmented images using ImageDataGenerator
+    augmented_img = next(datagen.flow(img, batch_size=1))
+    # Remove the batch dimension
+    augmented_img = np.squeeze(augmented_img, axis=0)
+    return augmented_img
