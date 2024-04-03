f54d185b0e24c1de72590fdf913a2f0aeddd69df
hs3458 <160627219+hs3458@users.noreply.github.com>
2024-04-03 12:15:01 -0400

    Update preprocess_images.py

--- code/preprocess_images.py
+++ code/preprocess_images.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 
 def preprocessor(img_path):
-        img = Image.open(img_path).convert("RGB").resize((224,224))
+        img = Image.open(img_path).convert("RGB").resize((192,192))
         img = (np.float32(img)-1.)/(255-1.)
-        img=img.reshape((224,224,3))
+        img=img.reshape((192,192,3))
         return img
